e6b4e45bf2bc914f5bd4e07ecf394222648596cd
Jeremy Poulin <jaypoulz@gmail.com>
2016-04-03 20:54:28 -0400

    Updated entity validations. Closes #1

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,4 +1,6 @@
 class Entity < ActiveRecord::Base
+  validates :domain, presence: true, uniqueness: { case_sensitive: false, scope: [ :port ] }
+
   belongs_to :user
   has_many :entity_token
   has_many :key, :through => :entity_token
